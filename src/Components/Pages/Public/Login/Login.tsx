748e6765c0c205ddfc0f9f8464fcbe6c7f06ceea
Aslan Mammadov <mammadovaslan21@gmail.com>
2023-01-28 01:17:12 +0400

    base url fix

--- src/Components/Pages/Public/Login/Login.tsx
+++ src/Components/Pages/Public/Login/Login.tsx
@@ -21,7 +21,7 @@ const Login = () => {
     try {
       const data = await axiosInstance.post(`/email-token`, {
         email,
-        base_url: "http://localhost:3000/token",
+        base_url: "https://mammadovaslan.github.io/E-commerce-project/token",
       });
     } catch (error) {
       console.log(error);
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,14 @@ import "./css/style.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {store} from './redux/store/store'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { store } from "./redux/store/store";
 import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <Provider store ={store}>
-    <Router>
+  <Provider store={store}>
+    <Router basename={process.env.PUBLIC_URL}>
       <App />
     </Router>
   </Provider>
