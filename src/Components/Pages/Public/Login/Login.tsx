b05e0e475180988855eac41acbe6c33950703924
Aslan Mammadov <mammadovaslan21@gmail.com>
2023-01-28 18:24:21 +0400

    base url changed

--- src/Components/Pages/Public/Login/Login.tsx
+++ src/Components/Pages/Public/Login/Login.tsx
@@ -21,7 +21,7 @@ const Login = () => {
     try {
       const data = await axiosInstance.post(`/email-token`, {
         email,
-        base_url: "https://trello-ecommerce.netlify.app/token",
+        base_url: "https://tello-ecommerce.vercel.app/token",
       });
     } catch (error) {
       console.log(error);
